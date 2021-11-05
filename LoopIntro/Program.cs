d44c211354e87cf6e50d929653372bedd2164fef
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2021-11-05 12:49:24 -0500

    for loops

--- LoopIntro/Program.cs
+++ LoopIntro/Program.cs
@@ -6,7 +6,21 @@ namespace LoopIntro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 1; i <= 10; i++)
+            {
+                if ( i % 2 ==0 )
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            for (int i = 10; i >= 1; i--)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
